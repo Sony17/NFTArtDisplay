ab2630b90f5f0872a6649c8cf66933a95c3e68b4
Sony <sohni2012@gmail.com>
2022-06-22 13:48:43 +0530

    UI changes

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import './App.css';
+import data from './data'
+import React from 'react';
+
+
+export default function App(props) {
+
+  
+
+  
+  return (
+    <div className="box">
+    <h2 className='emoji'>{props.emoji}</h2>
+    <h1 className='title'>{props.name}'s Generative Art NFT</h1>
+    <div className="grid">
+    { 
+        data.map(photo => {
+        return (
+          <div  className="item">
+            <img
+              className="img"
+              alt={"images"}
+              src={photo.img}
+            />
+            <div className="caption ">
+              <span className="credits">Photo by Sony
+             
+                <span> on </span> 
+                <a href={photo.img} target="_blank">  Deep Dream Generator
+                  </a>
+              </span>
+            </div>
+          </div>
+          );
+      }) }
+    </div>
+    <div className='footer'>
+    {/* <a href="mailto:sohni2012@gmal.com" class="footer__link">sohni2012@gmail.com</a> */}
+    <li class="social-list__item">
+                    <a class="social-list__link" href="https://www.linkedin.com/in/sonyy/"  target="_blank">
+                        <i class="fab fa-linkedin"></i>
+                    </a>
+                </li>
+                <li class="social-list__item">
+                    <a class="social-list__link" href="https://www.instagram.com/sony.nft/"  target="_blank">
+                        <i class="fab fa-instagram"></i>
+                    </a>
+                </li>            
+    </div>
+  </div>
+  );
+}
+
+
